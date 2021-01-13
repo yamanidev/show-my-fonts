3775c8054f44a043d52d8fa028fc24c7b094b19b
Drakkkath <elamine.yamani@gmail.com>
2021-01-13 07:33:57 +0100

    Added a counter

--- app.py
+++ app.py
@@ -17,14 +17,17 @@ font_style = Font(family = current_font, size = 20)
 
 def update():
     global i
+    global font_number
     i += 1
     current_font = fonts[i]
     font_style = Font(family = current_font, size = 20)
     current_font_label.config(text = current_font)
     dummy_text.config(font = font_style)
+    font_number.config(text = str(i+1) + "/ " + str(len(fonts)))
     current_font_label.pack()
     dummy_text.pack()
-    
+
+font_number = Label(root, text = str(i+1) + "/ " + str(len(fonts)))
 current_font_label = Label(root, text = current_font)
 dummy_text = Label(root, text = "I am a dummy text", font = font_style)
 next = Button(root, text = "Next", width = 30, command = update)
@@ -34,6 +37,7 @@ next = Button(root, text = "Next", width = 30, command = update)
 
 current_font_label.pack(pady = 30)
 dummy_text.pack()
+font_number.pack()
 next.pack(pady = 30)
 
 root.resizable(0, 0)
