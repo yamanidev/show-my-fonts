964c56ef997e4a137f695c87deb3b62ce3926e4b
yamanidev <elamine.yamani@gmail.com>
2022-02-11 18:18:03 +0100

    Added comments

--- src/app.py
+++ src/app.py
@@ -9,19 +9,22 @@ except ImportError:
     from tkinter import font
     from tkinter import ttk
 
-root = tk.Tk()
-
 FONTS = sorted(set(font.families()))
 
-dummy_text = tk.Label(root, text="I am cool dummy text", font=(FONTS[0], 20))
+root = tk.Tk()
+
+# Label for dummy text
+dummy_text = tk.Label(root, text="I am a cool dummy text", font=(FONTS[0], 20))
 dummy_text.place(relx=0.5, rely=0.2, anchor=tk.CENTER)
 
 
+# Callback function whenever combobox selection is changed
 def change_font(event=None):
     new_font = font.Font(root=root, family=event.widget.get(), size=20)
     dummy_text.config(font=new_font)
 
 
+# Combobox for the system's fonts
 fonts_combobox = ttk.Combobox(root, values=FONTS, state="readonly", width=30, font="Helvetica 12")
 fonts_combobox.current(0)
 fonts_combobox.bind("<<ComboboxSelected>>", change_font)
